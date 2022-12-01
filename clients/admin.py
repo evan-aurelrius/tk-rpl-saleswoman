273c1073a1a2a48239722ad5c764935d33eca5a3
ana117 <63540951+ana117@users.noreply.github.com>
2022-12-01 16:10:53 +0700

    Add client and temporary sales model

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Client, MockSales
 
-# Register your models here.
+admin.site.register(Client)
+admin.site.register(MockSales)
--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class MockSales(models.Model):
+    username = models.CharField(max_length=50)
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    information = models.TextField()
+    sales = models.ForeignKey(MockSales, on_delete=models.CASCADE, related_name='clients')
+
+    def __str__(self):
+        return self.name
+
