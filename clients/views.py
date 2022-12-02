68bb472d42b2f3fa03c152569309b55c30ec61bc
ana117 <63540951+ana117@users.noreply.github.com>
2022-12-02 14:12:02 +0700

    Remove unused imports

--- clients/views.py
+++ clients/views.py
@@ -1,8 +1,5 @@
-import json
-
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
-from django.views.decorators.csrf import csrf_exempt
 from django.db import IntegrityError
 
 from clients.forms import ClientForm
