a2caff36426ad59b1122959dd222dddfb40a6b0b
joyamanda <joya.ruth@ui.ac.id>
2022-12-08 15:28:12 +0700

    fixing error

--- highlight/forms.py
+++ highlight/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from product.models import *
 
-def HighlightForm() :
+def get_choices_product() :
     product_list = Product.objects.all()
     res = []
     for i in product_list :
@@ -9,5 +9,5 @@ def HighlightForm() :
         res.append(pair)
     return res
 
-# class HighlightForm(forms.Form):
-    # product_list = forms.CharField(widget=forms.Select(choices=get_choices_product()))
+class HighlightForm(forms.Form):
+    product_list = forms.CharField(widget=forms.Select(choices=get_choices_product()))
